484548ba90846ffe7e7b804ddeb351858b98b195
Daniel Hermansson <dhermansson@keyfactor.com>
2022-01-11 15:50:56 +0100

    Fix profile src

--- src/main/se/w3t/site/pages/team.cljs
+++ src/main/se/w3t/site/pages/team.cljs
@@ -32,9 +32,9 @@
                               :cols 2
                               :rowHeight 221}
                              (team-item/ui-team-item {:name "Daniel Hermansson"
-                                                      :source "https://scontent.fbma1-1.fna.fbcdn.net/v/t1.6435-9/89516747_10218825505562694_8147537910946594816_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=x0uZxX2igAMAX992V3h&_nc_ht=scontent.fbma1-1.fna&oh=18213dc4eabb34432c5ac7b6357b7adf&oe=61BB70EA"
+                                                      :source "https://storageapi.fleek.co/zertan-team-bucket/profiles/daniel.jpg"
                                                       :github "zertan"})
                              (team-item/ui-team-item
                               {:name "Robin Josef"
-                               :source "https://scontent.fbma1-1.fna.fbcdn.net/v/t31.18172-8/15972450_10206741876619347_5411572302697033139_o.jpg?_nc_cat=109&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=InJ2LuOcsrwAX9ugKdv&_nc_ht=scontent.fbma1-1.fna&oh=f109ad4bc6fa83aabddd3bf6ec5f8970&oe=61BD5D85"
+                               :source "https://storageapi.fleek.co/zertan-team-bucket/profiles/robin.jpg"
                                :github "16pj"}))))
